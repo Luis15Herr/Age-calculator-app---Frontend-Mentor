0b595ae2776d78f75221555213b87ad6a959c215
luis15herr <herrera05luis@gmail.com>
2023-11-12 01:41:08 -0400

    Js for the calc

--- js.js
+++ js.js
@@ -20,3 +20,55 @@ Vue.createApp({
     };
   },
 }).mount("#app");
+
+let meses = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
+
+let actualDay = 12;
+let actualMonth = 11;
+let birthDay = 15;
+let dayCounter = null;
+let birthMonth = 5;
+let stopDate = null;
+let daysTranscurred = 0;
+let monthsTranscurred = 0;
+
+for (let index = birthMonth; index < actualMonth + 1; index++) {
+  if (index === birthMonth) {
+    console.log("una vez");
+    dayCounter = birthDay;
+  }
+  monthsTranscurred++;
+
+  stopDate = meses[index] + 1;
+  if (index != birthMonth) {
+    dayCounter = 1;
+  } else {
+    dayCounter = dayCounter + 1;
+  }
+
+  if (index === actualMonth) {
+    stopDate = actualDay + 1;
+    if (actualDay < birthDay) {
+      monthsTranscurred--;
+    }
+  }
+
+  for (let j = dayCounter; j < stopDate; j++) {
+    daysTranscurred++;
+  }
+}
+console.log(monthsTranscurred);
+console.log(daysTranscurred);
